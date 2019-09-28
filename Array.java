6838a854694defc94ee11280f66b228bedaa98af
Rosgard <31376236+Rosgard@users.noreply.github.com>
2019-09-28 09:30:29 +0300

    LabaNum1FirstTry
    Не просмотрена(исправлений нету)

---
+++ Array.java
@@ -0,0 +1,69 @@
+package com.company;
+import java.util.Scanner;
+public class Array {
+    private int n;
+    private int x[];
+    //Неправильный ввод
+    Array(int n) throws Exception {
+        if (n <= 0) throw new Exception(new String("Unsupported number of elements!"));
+        this.n = n;
+        this.x = new int[this.n];
+    }
+    //Randomizer
+    void randInput(long min, long max) {
+        for (int i = 0; i < n; i++) {
+            x[i] = (int) (Math.random() * ++max + min);
+        }
+    }
+    //Keyboard input
+    void KeyboardInput(){
+        System.out.println("Your Massive: ");
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            x[i] = in.nextInt();
+        }
+    }
+    //RL
+    void OutputRightoLefto() {
+        System.out.println("(Massive output from right to left)");
+        for (int i = n-1; i >= 0; i--) {
+            System.out.println(x[i]);
+        }
+    }
+    //LR
+    void OutputLefoRighto() {
+        System.out.println("(Massive output from left to right )");
+        for (int i:x) {
+            System.out.println(i);
+        }
+    }
+    //Finder
+    void find(long z) {
+        int ind = -1;
+        for ( int i = 0; i < n; i++) {
+            if (x[i]==z) {
+                ind = i;
+                break;
+            }
+        }
+        if ( ind == -1)
+            System.out.println("Number " + z + " is absent.");
+        else
+            System.out.println("Number " + z + " is here!. It's index is = " + ind);
+    }
+    //Swap
+    void Swap(int first, int second){
+        int hi = x[first];
+        x[first] = x[second];
+        x[second] = hi;
+    }
+    //Bubble
+    void bubble() {
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (x[j] > x[j + 1])
+                    Swap(j, j + 1);
+            }
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,25 +1,51 @@
 package com.company;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
-
-        Student student = new Student();
-
-        student.age = 19;
-        student.course = 2;
-        student.name = "Iliya Petkevich";
-        student.isAlive = true;
-
-        System.out.println(student);
+        TestArray();
     }
-}
-class Student{
-    public String name;
-    public int age;
-    public int course;
-    public boolean isAlive;
-    public String toString()
-    {
-        return this.name + ", " + this.age + " y.o. ";
+
+    public static void TestArray() {
+        int n;
+        int num = 0;
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Input the dimension of the massive : ");
+            n = in.nextInt();
+            Array array = new Array(n);
+
+            while (num != 2 && num != 1) {
+                System.out.println("In what way do you want to fill the massive");
+                System.out.println("1. With randoms.");
+                System.out.println("2. By yourself from keyboard.");
+                System.out.print("Chosen number: ");
+                num = in.nextInt();
+                switch (num) {
+                    case 1:
+                        array.randInput(10, 150);
+                        array.OutputLefoRighto();
+                        array.OutputRightoLefto();
+                        break;
+                    case 2:
+                        array.KeyboardInput();
+                        array.OutputLefoRighto();
+                        array.OutputRightoLefto();
+                        break;
+                }
+            }
+
+            System.out.println("Input a number to find in the massive: ");
+            long z = in.nextInt();
+            array.find(z);
+            System.out.print("Massive sorted by bubble algorithm by increasing : ");
+            array.bubble();
+            array.OutputLefoRighto();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
 }
